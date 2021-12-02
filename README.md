e83bd05f380bf31fe1fd5ad7a8a902510ccd578d
STSC <STSC@Lenovo-127919.ad.csueastbay.edu>
2021-12-02 01:49:34 -0800

    done

--- README.md
+++ README.md
@@ -115,4 +115,66 @@ No modifications
     }
 }
 ```
+## (4)
+### (a)
+### (b)
+```javascript
+import React from 'react';
 
+class Activities extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { activities:[]}
+    }
+
+componentDidMount() {
+      
+    fetch('/activities').then(res=>res.json()).then((activities)=>{
+        this.setState({
+            ...this.state,
+            activities: activities
+        })
+    })
+}
+
+render(){
+    console.log(this.state.activities);
+    let tableRows = this.state.activities.map((events1) => {
+        return <tr>
+                  <td>{events1.name}</td>
+                  <td>{events1.dates}</td>
+              </tr>;
+    })
+      // Create a table using rows from above
+     let myTable = <table>
+                       <thead>
+                           <tr>
+                               <th>Name</th><th>Date(s)</th></tr>
+                         </thead>
+                            <tbody id="ActTable">
+                             {tableRows}
+                            </tbody>
+                     </table>;
+    return(
+            <>
+                <div id="actvitytable">
+                      <h1>Club Activities </h1>
+           
+                    <h2>Activities Scheduled</h2>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul> 
+                 <h2> Activity Table fetched from server</h2>
+                      {myTable}
+                </div>
+            </>
+    );
+}
+
+}
+
+export default Activities
+```
+![](images/image12_4b.png)
--- clubReact/activities.js
+++ clubReact/activities.js
@@ -1,30 +1,57 @@
-import React from "react";
-import ReactDOM from "react-dom";
-function Activities(props){
-    let tableC = props.events.map((e) =>{
-        return (<><tr>
-            <td> {e.name} </td>
-            <td> {e.dates} </td>
-            </tr></>)
-    });
-    return (<>
-    <main>
-        <table id="activity_table">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Date</th>
-        
-      </tr>
-    </thead>
-    <tbody>
-        {tableC}
-    </tbody>          
-  </table>
-  </main>
-  <footer>
-      &copy;2021 - Coding Club
-  </footer></>)
+import React from 'react';
+
+class Activities extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { activities:[]}
+    }
+
+componentDidMount() {
+      
+    fetch('/activities').then(res=>res.json()).then((activities)=>{
+        this.setState({
+            ...this.state,
+            activities: activities
+        })
+    })
+}
+
+render(){
+    console.log(this.state.activities);
+    let tableRows = this.state.activities.map((events1) => {
+        return <tr>
+                  <td>{events1.name}</td>
+                  <td>{events1.dates}</td>
+              </tr>;
+    })
+      // Create a table using rows from above
+     let myTable = <table>
+                       <thead>
+                           <tr>
+                               <th>Name</th><th>Date(s)</th></tr>
+                         </thead>
+                            <tbody id="ActTable">
+                             {tableRows}
+                            </tbody>
+                     </table>;
+    return(
+            <>
+                <div id="actvitytable">
+                      <h1>Club Activities </h1>
+           
+                    <h2>Activities Scheduled</h2>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul> 
+                 <h2> Activity Table fetched from server</h2>
+                      {myTable}
+                </div>
+            </>
+    );
+}
 
 }
-export default Activities;
+
+export default Activities
